fbb9718d283be61c6f391d4166d04fe26c65d9dd
guilhermear <guilhermear@fastcompras.com.br>
2019-06-24 11:49:31 -0300

    Inserindo pastas da view.

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const path = require('path');
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
-                                
+
+app.get('/terminal_WebShell.html', function(req, res){
+  res.sendFile(path.join(__dirname+'/terminal_WebShell.html'));
+});
+
+// app.set('images', __dirname + '/images');
+app.use('/images', express.static('images'));
+app.use('/css', express.static('css'));
+app.use('/fonts', express.static('fonts'));
+app.use('/js', express.static('js'));
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Umbler listening on port %s', port);
