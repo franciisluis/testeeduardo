1ed3bc10d88c6c919b759edd338bd9f8ad5171e9
guilhermear <guilhermear@fastcompras.com.br>
2019-06-24 11:34:53 -0300

    Inserindo gitignore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
                                 
 var port = process.env.PORT || 3000;
